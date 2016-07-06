ba6ed710143b2785d9806a9d069c929c18e997c0
Spencer814 <andre.d.newman@gmail.com>
2016-07-05 23:02:54 -0400

    Style changes, and added carousel

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,13 +2,6 @@ angular.module('starter.controllers', [])
 
 .controller('WbaCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on Wba start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
   // Form data for the login modal
   $scope.loginData = {};
 
